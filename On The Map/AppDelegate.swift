31661c7d51676a6d6601948e829e8dd03982dc70
Jack Ngai <jackngai@gmail.com>
2016-10-15 23:26:03 -0400

    Deleted bridging header file. Added Import statement for FBSDKLoginKit for each swift file that needed it.

--- On The Map/AppDelegate.swift
+++ On The Map/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- On The Map/LoginViewController.swift
+++ On The Map/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
     
